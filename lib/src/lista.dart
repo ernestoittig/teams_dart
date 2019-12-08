629513825fa195a1dbaddcbbf4564084cb94b078
Ernesto Ittig <e.ittig@gmail.com>
2019-12-08 16:14:23 -0300

    Document and comment library

--- lib/src/lista.dart
+++ lib/src/lista.dart
@@ -1,18 +1,27 @@
 import 'dart:math' show Random;
 
+/// A list of people to be sorted into teams
 class Lista {
   final List<String> _list;
 
+  /// Creates a new instance with the people from [list]
   Lista(List<String> list): _list = list;
 
+  /// Creates the list from [str]
+  /// 
+  /// The values are split in line feed characters and all empty lines and whitespace is ignored
   Lista.fromString(String str)
       : _list = str.split('\n').where((v) => !RegExp(r'^\s*$').hasMatch(v)).toList();
   
+  /// Get a random person from the list of people
+  /// 
+  /// If [del] is true the person chosen will also be deleted from the list
   String getRandom([bool del = false]) {
     final index = Random().nextInt(_list.length);
     if (del) return _list.removeAt(index);
     return _list[index];
   }
 
+  /// Get a copy of the list of people
   List<String> get list => _list.toList();
 }
--- lib/teams_dart.dart
+++ lib/teams_dart.dart
@@ -2,12 +2,15 @@ import 'src/lista.dart';
 
 export 'src/lista.dart';
 
+/// Creates a list of evenly distributed teams
 List<List<String>> makeTeams(Lista lista, int numberOfTeams) {
   final teams = <List<String>>[];
+  // We make a copy just to make sure we aren't modifying any variables the user passes to us
   var copyN = numberOfTeams;
   final copyL = Lista(lista.list);
   while (copyN > 0 && copyL.list.isNotEmpty) {
     final team = <String>[];
+    // We create a new variable because the length of the list will change with each iteration
     final numberOfPeople = copyL.list.length;
     for (var i = 0; i < (numberOfPeople / copyN).floor(); ++i) {
       team.add(copyL.getRandom(true));
